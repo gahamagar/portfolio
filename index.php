7cb3e6a1c44a35ed972c62b9bd7a59cd7bb56e37
Ganesh Gaha <iamganeshgaha@gmail.com>
2021-04-01 09:29:09 +0545

    my portfolio update

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GANESH IT Professional & Web Developer</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <!-- starting of header section -->
+    <header>
+        <div class="menu-toggler">
+            <div class="bar half start"></div>
+            <div class="bar"></div>
+            <div class="bar half end"></div>
+        </div>
+        <nav class="top-nav">
+            <ul class="nav-list">
+                <li><a href="index.html" class="nav-link">Home</a></li>
+                <li><a href="#about" class="nav-link">About</a></li>
+                <li><a href="#services" class="nav-link">Services</a></li>
+                <li><a href="#portfolio" class="nav-link">Portfolio</a></li>
+                <li><a href="#experinces" class="nav-link">Experiences</a></li>
+                <li><a href="#contact" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="landing-text">
+            <h6>I am <span class="typed-words"> &nbsp;</span> </h6>
+            <h1>Ganesh Gaha Magar</h1>
+        </div>
+        <!-- ending of header section  -->
+        <!-- starting of about section  -->
+    </header>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="profile-img">
+                <img src="images/ganesh.jpg" alt="">
+            </div>
+            <div class="about-details">
+                <div class="about-heading">
+                    <h1>About</h1>
+                    <h6>Myself</h6>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Fuga necessitatibus et magnam ipsum repellendus
+                    sapiente voluptatem illo suscipit repudiandae quaerat qui,
+                    iusto sint eligendi minima quibusdam nesciunt officia quo neque.
+
+                </p>
+                <div class="social-media">
+                    <ul class="nav-list">
+                        <li>
+                            <a href="https://www.facebook.com/gahamagar55" class="icon-link">
+                                <i class="fa fa-facebook-square"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="icon-link">
+                                <i class="fa fa-linkedin-square text-blue"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="icon-link">
+                                <i class="fa fa-youtube-square"></i> </a>
+                        <li>
+                            <a href="#" class="icon-link"></a>
+                            <i class="fa fa-twitter-square"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="services" id="services">
+        <div class="container">
+            <div class="section-heading">
+                <h1>Services</h1>
+                <h6>What can i do for You?</h6>
+            </div>
+            <div class="my-skills">
+                <div class="skill">
+                    <div class="icon-container">
+                        <i class="fa fa-layer-group"></i>
+                    </div>
+                    <h1>Web Design</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div class="skill">
+                    <div class="icon-container">
+                        <i class="fa fa-code"></i>
+                    </div>
+                    <h1>Web Developer</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div class="skill">
+                    <div class="icon-container">
+                        <i class="fa fa-bar-chart"></i>
+                    </div>
+                    <h1>Product Strategy</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="portfolio" id="portfolio">
+        <div class="container">
+            <div class="section-heading">
+                <h1>Portfolio</h1>
+                <h6>View some of my recent work</h6>
+            </div>
+            <div class="portfolio-item">
+                <div class="portfolio-img">
+                    <img src="images/portitem1.jpeg" alt="">
+                </div>
+                <div class="portfolio-description portfolio">
+                    <h6>Web Development</h6>
+                    <h1>Blog website</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        
+                    </p>
+                    <a href="#" class="cta">View Details</a>
+                </div>
+            </div>
+
+            <div class="portfolio-item">
+                <div class="portfolio-description portfolio">
+                    <h6>Web Design</h6>
+                    <h1>Product Layout</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                    <a href="#" class="cta">View Details</a>
+                </div>
+                <div class="portfolio-img portfolio">
+                    <img src="images/portitem2.jpeg" alt="">
+                </div>
+            </div>
+
+            <div class="portfolio-item">
+                <div class="portfolio-img">
+                    <img src="images/portitem3.jpeg" alt="">
+                </div>
+                <div class="portfolio-description portfolio">
+                    <h6>Web Design</h6>
+                    <h1>Product sketch</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                    <a href="#" class="cta">View Details</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="experience" id="experience">
+        <div class="container">
+            <div class="section-heading">
+                <h1>Work Experience</h1>
+                <h6>Past and Current Jobs</h6>
+            </div>
+            <div class="timeline">
+                <ul>
+                    <li class="date" date-date="2019-2020">
+                        <h1>Ezee Computer institute and Training</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </li>
+                    <li class="date" date-date="2017-present">
+                        <h1>Nc_link Pvt.ltd</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </li>
+                    <li class="date" date-date="2017-present">
+                        <h1>Microsoft</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="contact" id="contact">
+        <div class="container">
+            <div class="section-heading">
+                <h1>Contact</h1>
+                <h6>Let's work together</h6>
+            </div>
+            <form action="">
+                <label for="name">Name:</label>
+                <input type="text" id="name" placeholder="Enter Your Name" required>
+                <label for="email">Email:</label>
+                <input type="text" id="name" placeholder="Enter Your Email" required>
+                <label for="services" required>Services:</label>
+                <select name="services" id="services">
+                    <option value="">Web Developer</option>
+                    <option value="">Web Design</option>
+                    <option value="">Web Design/Development</option>
+                </select>
+                <label for="subject">Subject:</label>
+                <textarea name="subject" id="subject" cols="10" rows="10" placeholder="write your message"></textarea>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </section>
+    <footer class="copyright">
+        <div class="up" id="up">
+            <i class="fa fa-chevron-up"></i>
+        </div>
+        <p> &copy; 2020 *All Copyright Reserved <a href="#">Ganesh Gaha Magar</a> </p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="typed.js"></script>
+    <script>
+        var typed = new Typed('.typed-words', {
+            strings: [" ", "an IT Student", " ISP Network Admin", "Freelancer"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            backDelay: 1000,
+            startDelay: 1000,
+            loop: true,
+            showCursor: true
+        });
+    </script>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
---
+++ main.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('#up').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+    });
+
+    // $('.top.nav nav-link').on('click', function (){
+    //     $('.menu-toggler').removeClass('open');
+    //     $('.top-nav').removeClass('open');
+    // });
+
+    // $('nav a[href*="#"]').on('click', function (){
+    //     $('html, body').animate( keyframes:{
+    //         scrollTop: $($(this).attr('href')).offset().top -100
+    //     }, options:2000);
+    // }); 
+});
+
+
